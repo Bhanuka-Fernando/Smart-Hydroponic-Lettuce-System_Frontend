3e85c1347dfeb81659e4e2a500a4f8676483b237
Bhanuka Fernando <bhanuka.fernando@arimaclanka.com>
2026-03-03 08:43:53 +0530

    solved the app navigator issue

--- mobile-app/src/utils/constants.ts
+++ mobile-app/src/utils/constants.ts
@@ -1,5 +1,3 @@
-export const API_BASE_URL = "http://192.168.43.226:8000";
-export const ML_BASE_URL = "http://192.168.43.226:8001";
-export const SPOILAGE_BASE_URL = "http://192.168.43.226:8002";
-
-
+export const API_BASE_URL = "http://172.20.10.12:8000";
+export const ML_BASE_URL = "http://172.20.10.12:8001";
+export const SPOILAGE_BASE_URL = "http://172.20.10.12:8002";
