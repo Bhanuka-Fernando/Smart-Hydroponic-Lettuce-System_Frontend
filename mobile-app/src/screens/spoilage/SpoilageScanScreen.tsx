51a5f292f90d17153e7fc6dad17f7ac6d1725580
Bhanuka Fernando <bhanuka.fernando@arimaclanka.com>
2026-03-08 18:05:29 +0530

    removed dataset in scan screen

--- mobile-app/src/screens/spoilage/SpoilageScanScreen.tsx
+++ mobile-app/src/screens/spoilage/SpoilageScanScreen.tsx
@@ -694,7 +694,7 @@ Image: ${sample.image_name ?? "none"}`
             >
               <Ionicons name="camera-outline" size={16} color={PRIMARY} />
               <Text className="ml-2 text-[12px] font-extrabold text-gray-900">
-                Simulate Camera (Dataset)
+                Simulate Camera
               </Text>
             </TouchableOpacity>
 
